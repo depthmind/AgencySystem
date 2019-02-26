3dbcc06faef13ed7ccd481e12997a063f5ed4c4c
kongzheng1993 <kongzheng1993@hotmail.com>
2019-02-26 22:14:07 +0800

    git test

--- src/com/agency/crm/entity/AgencyBase.java
+++ src/com/agency/crm/entity/AgencyBase.java
@@ -32,7 +32,17 @@ public class AgencyBase extends BaseBean<AgencyBase> {
 	private String licence2ImagePath;
 	private Date expiryDate;
 	private String validPeriod;
-	
+	private String pic;
+
+
+	public String getPic() {
+		return pic;
+	}
+
+	public void setPic(String pic) {
+		this.pic = pic;
+	}
+
 	public Date getExpiryDate() {
 		return expiryDate;
 	}
